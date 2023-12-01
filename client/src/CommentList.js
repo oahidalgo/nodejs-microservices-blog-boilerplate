48ba9334482d506cb7a3ff0f086c532b2ca662a9
oahidalgo <ohgovilla@gmail.com>
2023-12-01 15:08:55 -0600

    ADD - Dockerfile files to client and comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,26 +1,25 @@
 import React from "react";
 
-export default ({ comments }) => {
-    
-    const renderedComments = comments.map(comment => {
-        let content 
-        
-        if (comment.status === 'approved'){
-            content = comment.content
-        }
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
 
-        if (comment.status === 'pending'){
-            content = 'This comment is awaiting moderation'
-        }
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
 
-        if (comment.status === 'rejected'){
-            content = 'This comment has been rejected'
-        }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
 
-        return <li key={comment.id}>{content}</li>
-    })
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
 
-    return <ul>
-        {renderedComments}
-    </ul>
-}
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
