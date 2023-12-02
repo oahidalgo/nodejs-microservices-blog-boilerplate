f170f9721203285b8dd1aa6bfb3e4d266d839123
oahidalgo <ohgovilla@gmail.com>
2023-12-01 19:56:36 -0600

    ADD - Dockerfiles for each svc

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-const events = []
+const events = [];
 
-app.post('/events', (req, res) => {
-    const event = req.body
-    events.push(event);
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
-        console.log(err.message);
-      });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
-        console.log(err.message);
-      });
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-        console.log(err.message);
-      });
-    axios.post('http://localhost:4003/events', event).catch((err) => {
-        console.log(err.message);
-      });
+  events.push(event);
 
-    res.send({ status: 'OK' })
-})
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
 
-app.get('/events', (req,res)=> {
-    res.send(events)
-})
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log('listening in 4005')
-})
+  console.log("Listening on 4005");
+});
--- query/index.js
+++ query/index.js
@@ -1,59 +1,64 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const axios = require('axios')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {}
+const posts = {};
 
 const handleEvent = (type, data) => {
-    if (type === 'PostCreated'){
-        const { id, title } = data
-        posts[id] = {id, title, comments: []}
-    }
+  if (type === "PostCreated") {
+    const { id, title } = data;
 
-    if (type === 'CommentCreated'){
-        const { id, content, postId, status } = data
+    posts[id] = { id, title, comments: [] };
+  }
 
-        const post = posts[postId]
-        post.comments.push({ id, content, status})
-    }
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
 
-    if(type === 'CommentUpdated'){
-        const { id, content, postId, status } = data
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
 
-        const post = posts[postId]
-        const comment = post.comments.find(comment => {
-            return comment.id === id
-        })
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
 
-        comment.status = status
-        comment.content = content
-    }
-}
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body
-    
-    handleEvent(type,data)
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-    res.send({})
-})
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
+  res.send({});
+});
 
 app.listen(4002, async () => {
-    console.log('Listening on 4002')
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://event-bus-srv:4005/events");
 
-    const res = await axios.get('http://localhost:4005/events')
-    for (let event of res.data){
-        console.log('Processing event:', event.type)
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
 
-        handleEvent(event.type, event.data)
+      handleEvent(event.type, event.data);
     }
-})
+  } catch (error) {
+    console.log(error.message);
+  }
+});
